924910960c617da6fb318c5d74875f4fb1d34e78
Esteban Trujillo <esteban.truillo101@alu.ulpgc.es>
2025-03-24 17:42:01 +0000

    Carga dinámica comentarios de usuarios

---
+++ js/user-comment.js
@@ -0,0 +1,30 @@
+function userComment() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const videogameId = urlParams.get('id');
+
+    fetch('/database.json')
+        .then(response => response.json())
+        .then(data => {
+            const commentsContainer = document.getElementById('comentaries');
+            const comments = data.comments.filter(comment => comment.videogameId == videogameId);
+
+            comments.forEach(comment => {
+                fetch('/templates/comentaries.html')
+                    .then(response => response.text())
+                    .then(template => {
+                        const commentElement = document.createElement('div');
+                        commentElement.innerHTML = template;
+                        const user = data.users.find(user => user.id == comment.userId);
+                        commentElement.querySelector('.info h3').innerText = user.name;
+                        commentElement.querySelector('.info p').innerText = comment.content;
+                        commentElement.querySelector(`input#star${comment.rating}`).checked = true;
+                        const profileImage = commentElement.querySelector('.avatar');
+                        profileImage.src = user.profileicon;
+                        profileImage.alt = `${user.name}'s profile picture`;
+                        commentElement.querySelector('#userlink1').href = `userprofile.html?id=${user.id}`;
+                        commentElement.querySelector('#userlink2').href = `userprofile.html?id=${user.id}`;
+                        commentsContainer.appendChild(commentElement);
+                    });
+            });
+        });
+}
