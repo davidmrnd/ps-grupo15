734a241687044dbf352b6c8b9b4a94804767c20a
Agus <aagiribar@yahoo.es>
2025-04-01 12:29:58 +0100

    Añadido middleware para la gestión de las peticiones CORS

--- js/backend/express.js
+++ js/backend/express.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const apiFunctions = require('./api/api.cjs');
 const keys = require('./api/keys.json');
 const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(418).send('GameCritic backend working');
