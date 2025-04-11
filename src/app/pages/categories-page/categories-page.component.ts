84fba1d8132ef3e68886332f7052fe85daf31732
Esteban Trujillo <esteban.truillo101@alu.ulpgc.es>
2025-04-11 16:36:19 +0100

    categories-page

---
+++ src/app/pages/categories-page/categories-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {CarouselComponent} from '../../components/carousel/carousel.component';
+
+@Component({
+  selector: 'app-categories-page',
+  imports: [
+    CarouselComponent
+  ],
+  templateUrl: './categories-page.component.html',
+  styleUrl: './categories-page.component.css'
+})
+export class CategoriesPageComponent {
+
+}
