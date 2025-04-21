15a5a26764f985362916d1efb8a0c2606341bda7
AlejandroGuerraJimenez <alejandro.guerra111@alu.ulpgc.es>
2025-04-21 18:15:42 +0100

    Carga dinamica de las estrellas de los comentarios

--- src/app/pages/videogameprofile-page/videogameprofile-page.component.ts
+++ src/app/pages/videogameprofile-page/videogameprofile-page.component.ts
@@ -1,16 +1,53 @@
-import { Component } from '@angular/core';
-import {ProfileComponent} from '../../components/profile/profile.component';
-import {StarsComponent} from '../../components/stars/stars.component';
-import {CommentariesComponent} from '../../components/commentaries/commentaries.component';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { DataService } from '../../services/data.service';
+import { ProfileComponent } from '../../components/profile/profile.component';
+import { StarsComponent } from '../../components/stars/stars.component';
+import { CommentariesComponent } from '../../components/commentaries/commentaries.component';
 
 @Component({
   selector: 'app-videogameprofile-page',
+  templateUrl: './videogameprofile-page.component.html',
+  styleUrls: ['./videogameprofile-page.component.css'],
+  standalone: true,
   imports: [
+    CommonModule,
     ProfileComponent,
     StarsComponent,
     CommentariesComponent
-  ],
-  templateUrl: './videogameprofile-page.component.html',
-  styleUrls: ['./videogameprofile-page.component.css']
+  ]
 })
-export class VideogamePageComponent {}
+export class VideogamePageComponent implements OnInit {
+  comments: any[] = [];
+  averageRating: number = 0;
+  videogameId!: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.videogameId = params['id'];
+      this.loadComments();
+    });
+  }
+
+  loadComments(): void {
+    this.dataService.getCommentsByVideogameId(this.videogameId).subscribe(comments => {
+      this.comments = comments;
+      this.calculateAverage();
+    });
+  }
+
+  calculateAverage(): void {
+    if (this.comments.length === 0) {
+      this.averageRating = 0;
+      return;
+    }
+    const total = this.comments.reduce((sum, c) => sum + c.rating, 0);
+    this.averageRating = total / this.comments.length;
+  }
+}
