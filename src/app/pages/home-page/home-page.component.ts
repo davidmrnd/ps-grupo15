d93f9868ac2d626eb00220039777b25896fa37e2
davidmrnd <informaticadavid4815@gmail.com>
2025-04-21 18:17:48 +0100

    boton empezar a valorar

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {WelcomeComponent} from "../../components/welcome/welcome.component";
 import { CarouselComponent } from "../../components/carousel/carousel.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   imports: [
     WelcomeComponent,
-    CarouselComponent
+    CarouselComponent,
+    RouterLink
 ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
