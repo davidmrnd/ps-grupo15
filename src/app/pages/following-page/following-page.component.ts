fd7cc5f835d90696d774e481a29783434337877e
davidmrnd <informaticadavid4815@gmail.com>
2025-04-25 18:29:35 +0100

    detalles visuales y de codigo en following page

--- src/app/pages/following-page/following-page.component.ts
+++ src/app/pages/following-page/following-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { DataService } from '../../services/data.service';
-import { Subscription } from 'rxjs';
 import { StarsComponent } from "../../components/stars/stars.component";
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
@@ -14,12 +13,11 @@ import { RouterLink } from '@angular/router';
 })
 export class FollowingPageComponent implements OnInit {
   comments: any[] = [];
-  private userSubscription: Subscription | null = null;
 
   constructor(private authService: AuthService, private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.userSubscription = this.authService.getCurrentUserObservable().subscribe((user) => {
+    this.authService.getCurrentUserObservable().subscribe((user) => {
       if (user) {
         this.dataService.getFollowingComments(user.uid).subscribe((comments) => {
           this.comments = [];
@@ -27,22 +25,15 @@ export class FollowingPageComponent implements OnInit {
           for (let comment of comments) {
             this.dataService.getUsersById(comment.userId).subscribe(user => {
               comment.user = user;
-              console.log(comment);
-              this.comments.push(comment);
             });
             this.dataService.getVideogameById(comment.videogameId).subscribe(videogame => {
               comment.videogame = videogame;
-              this.comments.push(comment);
             });
+            this.comments.push(comment);
           }
         });
       }
     });
   }
-  
-  ngOnDestroy(): void {
-    if (this.userSubscription) {
-      this.userSubscription.unsubscribe();
-    }
-  }
+
 }
