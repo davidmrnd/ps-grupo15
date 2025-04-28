baa08886ea43127f819226d7e3942eda713cb67a
Agustín Alfonso González Iríbar <agustin.gonzalez112@alu.ulpgc.es>
2025-04-28 16:40:32 +0100

    Implementada carga de perfil de videojuego a través de ID numérico

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,8 @@
 import {DataService} from '../../services/data.service';
 import {ActivatedRoute, RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, inject, Input, OnInit} from '@angular/core';
+import {ApiService} from '../../services/api.service';
 
 @Component({
   selector: 'app-profile',
@@ -14,7 +15,10 @@ export class ProfileComponent  implements OnInit {
 
   @Input() type: string = '';
   data: any = null;
+  gameInfo: any = null;
+  gameCover: any = null;
   id!: string;
+  apiService: ApiService = inject(ApiService);
 
   constructor(private dataService: DataService, private route: ActivatedRoute) {}
 
@@ -22,16 +26,30 @@ export class ProfileComponent  implements OnInit {
     this.route.queryParams.subscribe(params => {
       this.id = params['id'];
     });
-    console.log(this.id);
 
     if(this.type === 'videogame'){
+      this.apiService.getVideogameProfile(parseInt(this.id)).subscribe((response) => {
+        this.data = response;
+        this.gameInfo = response.apiResponse[0].result[0];
+        this.gameCover = response.apiResponse[1].result[0];
+      })
+      /**
       this.dataService.getVideogameById(this.id).subscribe(response => {
         this.data = response;
       });
+       **/
     }else{
       this.dataService.getUsersById(this.id).subscribe(response => {
         this.data = response;
       });
     }
+  }
+
+  protected getVideogameCoverURL() : string{
+    return `https://images.igdb.com/igdb/image/upload/t_cover_big/${this.gameCover.image_id}.jpg`
+  }
+
+  protected showReleaseYear() {
+    return !isNaN(this.apiService.getReleaseYear(this.gameInfo.first_release_date));
   }
 }
