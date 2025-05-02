054b14cacf20df041b5c8c1497a427afca6d0822
Agus <aagiribar@yahoo.es>
2025-05-03 00:29:34 +0100

    Implementados carruseles de forma que se muestre información obtenida de la API

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { DataService } from '../../services/data.service';
+import {ApiService} from '../../services/api.service';
 
 @Component({
   selector: 'app-carousel',
@@ -12,17 +12,34 @@ import { DataService } from '../../services/data.service';
 })
 export class CarouselComponent implements OnInit {
   @Input() category!: string;
-  data: any[] = [];
+  videogameData: any[] = [];
+  coverData: any[] = [];
+  coverURLS: string[] = [];
   @ViewChild('carouselTrack') carouselTrack!: ElementRef;
   visibleSlides: number = 4;
+  carouselGames = new Map<string, number[]>();
 
-  constructor(private dataService: DataService) {}
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.carouselGames.set("Novedades", [242408, 135400, 112875, 1020, 126290, 267306, 300976, 305152]);
+    this.carouselGames.set("Acción", [103054, 127044, 112875, 1020, 138949]);
+    this.carouselGames.set("Supervivencia", [10239, 135400, 1879, 7504, 126290]);
+    this.carouselGames.set("Disparos", [242408, 9509, 83728, 114795, 109096]);
+    this.carouselGames.set("Deportes", [308034, 19554, 138766, 308698, 24985]);
+
     if (this.category) {
-      this.dataService.getVideogames(this.category).subscribe(response => {
-        this.data = response.slice(0, 5); // ajustar el número de elementos a mostrar
-      });
+      const idList = this.carouselGames.get(this.category);
+      if (idList) {
+        this.apiService.getVideogameInfoForCorousel(idList).subscribe((response) => {
+          this.videogameData = response.apiResponse[0].result;
+          this.coverData = response.apiResponse[1].result;
+          for (const videogame of this.videogameData) {
+            const videogameCoverData = this.coverData.find((cover) => cover.game === videogame.id);
+            videogame.coverURL = `https://images.igdb.com/igdb/image/upload/t_1080p/${videogameCoverData.image_id}.jpg`
+          }
+        });
+      }
     }
     this.updateVisibleSlides();
   }
@@ -46,13 +63,13 @@ export class CarouselComponent implements OnInit {
 
     if (direction === 'prev') {
       if (currentTransform === 0) {
-        const maxTransform = -(slideWidth * (this.data.length - this.visibleSlides));
+        const maxTransform = -(slideWidth * (this.videogameData.length - this.visibleSlides));
         track.style.transform = `translateX(${maxTransform}px)`;
       } else {
         track.style.transform = `translateX(${Math.min(currentTransform + slideWidth, 0)}px)`;
       }
     } else if (direction === 'next') {
-      const maxTransform = -(slideWidth * (this.data.length - this.visibleSlides));
+      const maxTransform = -(slideWidth * (this.videogameData.length - this.visibleSlides));
       if (currentTransform === maxTransform) {
         track.style.transform = `translateX(0px)`;
       } else {
