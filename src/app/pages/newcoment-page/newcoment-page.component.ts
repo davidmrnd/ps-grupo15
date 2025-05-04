78fbbb015db36d4d3ab5208c5224cd90e6bdb7e0
Agus <aagiribar@yahoo.es>
2025-05-04 21:16:03 +0100

    Implementado mensaje de error al introducir un URL incorrecto en la página de añadir un comentario

--- src/app/pages/newcoment-page/newcoment-page.component.ts
+++ src/app/pages/newcoment-page/newcoment-page.component.ts
@@ -1,15 +1,17 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ProfileComponent } from '../../components/profile/profile.component';
 import { NewcomentComponent } from "../../components/newcoment/newcoment.component";
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {ApiService} from '../../services/api.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-newcoment-page',
   imports: [
     ProfileComponent,
-    NewcomentComponent
-],
+    NewcomentComponent,
+    NgIf
+  ],
   templateUrl: './newcoment-page.component.html',
   styleUrl: './newcoment-page.component.css'
 })
@@ -18,10 +20,13 @@ export class NewcomentPageComponent implements OnInit {
   protected gameInfo: any;
   protected videogameId: string = "";
   protected gameCover!: string;
+  showContent: boolean = false;
+  showErrorMessage: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private apiService: ApiService,
+    private router: Router,
   ) {
   }
 
@@ -31,13 +36,26 @@ export class NewcomentPageComponent implements OnInit {
       if (this.videogameSlug) {
         this.apiService.getVideogameProfileFromSlug(this.videogameSlug).subscribe((response) => {
           this.gameInfo = response.apiResponse[0];
-          this.videogameId = this.gameInfo.id.toString();
 
-          this.apiService.getCoverURL(parseInt(this.videogameId), "cover_big").subscribe((response) => {
-            this.gameCover = response.fullURL;
-          });
+          if (this.gameInfo) {
+            this.videogameId = this.gameInfo.id.toString();
+
+            this.apiService.getCoverURL(parseInt(this.videogameId), "cover_big").subscribe((response) => {
+              this.gameCover = response.fullURL;
+            });
+          }
+
+          else {
+            this.showErrorMessage = true;
+          }
+
+          this.showContent = true;
         });
       }
     });
+  }
+
+  navigateToExplore() {
+    this.router.navigate(['/categories']);
   }
 }
